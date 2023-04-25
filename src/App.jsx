3bd1cdcac8a5f8124bc4bf4a34fdca3517a3c80b
Thomas Notarangelo <thomas@macbook-pro-de-thomas.home>
2023-04-25 19:09:18 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Jobs from "./components/Jobs";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Jobs />
+      <Footer />
+    </div>
+  );
+}
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>Made with Reac at Le Reacteur by Poussin</p>
+    </footer>
+  );
+};
+
+export default Footer;
